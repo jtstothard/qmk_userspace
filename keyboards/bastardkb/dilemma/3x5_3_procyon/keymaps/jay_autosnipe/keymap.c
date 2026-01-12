e305cee0c60a1c56f0f863cc4e8f50d6a4f10f07
jtstothard <jtstothard@gmail.com>
2026-01-12 21:59:51 +0000

    Discard multiple frames after CPI change to fix cursor jump

--- keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/jay_autosnipe/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_3_procyon/keymaps/jay_autosnipe/keymap.c
@@ -148,25 +148,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // clang-format on
 
 #ifdef POINTING_DEVICE_ENABLE
-// Flag to track CPI changes - used to prevent cursor jump when entering sniping mode
-static bool g_cpi_changed = false;
+// Counter to track how many reports to discard after CPI change
+// Azoteq reports at ~10ms intervals, discard a few frames to clear buffer
+#define CPI_CHANGE_DISCARD_FRAMES 5
+static uint8_t g_cpi_discard_count = 0;
 
 #    ifdef DILEMMA_AUTO_SNIPING_ON_LAYER
 layer_state_t layer_state_set_user(layer_state_t state) {
     bool sniping_enabled = layer_state_cmp(state, DILEMMA_AUTO_SNIPING_ON_LAYER);
-    // Only set the flag if the sniping state actually changes
+    // Only set the counter if the sniping state actually changes
     if (sniping_enabled != dilemma_get_pointer_sniping_enabled()) {
-        g_cpi_changed = true;
+        g_cpi_discard_count = CPI_CHANGE_DISCARD_FRAMES;
     }
     dilemma_set_pointer_sniping_enabled(sniping_enabled);
     return state;
 }
 #    endif // DILEMMA_AUTO_SNIPING_ON_LAYER
 
-// Discard movement report after CPI change to prevent cursor jump
+// Discard movement reports after CPI change to prevent cursor jump
 report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
-    if (g_cpi_changed) {
-        g_cpi_changed = false;
+    if (g_cpi_discard_count > 0) {
+        g_cpi_discard_count--;
         mouse_report.x = 0;
         mouse_report.y = 0;
     }
